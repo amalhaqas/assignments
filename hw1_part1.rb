6a26d9d56ca329c70cd54a9ac82566d84521c158
amalhaqas <89962710+amalhaqas@users.noreply.github.com>
2021-09-03 00:52:27 +0300

    Add files via upload

---
+++ hw1_part1.rb
@@ -0,0 +1,44 @@
+# ####################################################################################################
+# ---------------------------------- START OF QUESTION 1 PART 1 --------------------------------------
+# ####################################################################################################
+
+# Find net salary for employee when the bonus rate is as follows
+#   basic salary between 0 and 10000 then bonus 25%.
+#   basic salary between 10001 and 100000 then bonus 15%.
+#   basic salary > 100000 then bonus 10%.
+# Print basic salary, bonus amount and net salary
+
+# ----------------------------------------------------------------------------------------------------
+
+basic_salary = 0
+bonus =0
+
+loop do
+       basic_salary = gets.to_i
+    if basic_salary.abs >=0 
+        basic_salary = basic_salary.abs
+        case
+        when (0..10000).include?(basic_salary)
+            bonus = basic_salary * 0.25
+        when (10001..100000).include?(basic_salary)
+            bonus = basic_salary * 0.15
+        when basic_salary > 100000
+            bonus = basic_salary * 0.10
+        end
+        break
+    end
+end
+
+
+
+net_salary = basic_salary + bonus
+
+print "Basic Salary : #{basic_salary}\n"
+print "Bonus : #{bonus}\n"
+print "Net Salary : #{net_salary}"
+
+# ####################################################################################################
+# ----------------------------------- END OF QUESTION 1 PART 1 ---------------------------------------
+# ####################################################################################################
+
+
